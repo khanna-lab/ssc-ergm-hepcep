4ee63d0fcef5209e52b8d78c1c427f806b8fb0f4
Aditya Khanna <khanna7.work@gmail.com>
2026-08-20 22:07:29 +0530

    set controls for runtime

--- exercises/03-sequential.R
+++ exercises/03-sequential.R
@@ -15,11 +15,16 @@ f_mix <- net ~ edges +
   nodemix("race.num", levels2 = -1) +
   nodematch("chicago")
 
+# Light, FIXED-size MCMC so each fit runs quickly at n = 1000. The key knob is
+# MCMC.effectiveSize = NULL: it turns off ergm's adaptive sample-size growth
+# (those "increasing sample size" messages). The fit is rougher but fast.
+ctrl <- control.ergm(MCMC.interval = 512, MCMC.samplesize = 512,
+                     MCMC.effectiveSize = NULL, MCMLE.maxit = 30)
+
 # ---- Run this: fit the mixing block, then warm-start ------------------------
-# (Fits take a few seconds each at n = 1000.)
 
 # 1. Fit the mixing block to its targets.
-fit_mix <- ergm(f_mix, target.stats = ts_mix, eval.loglik = FALSE)
+fit_mix <- ergm(f_mix, target.stats = ts_mix, control = ctrl, eval.loglik = FALSE)
 
 # 2. Simulate one network from that fit -> the "warm start" for the next fit.
 net_warm <- simulate(fit_mix, nsim = 1)
@@ -27,7 +32,7 @@ net_warm <- simulate(fit_mix, nsim = 1)
 # 3. Add an out-degree term, fitting FROM the warm start (net_warm on the LHS).
 fit5 <- ergm(update(f_mix, net_warm ~ . + odegree(0)),
              target.stats = c(ts_mix, odeg_target(0)),
-             eval.loglik = FALSE)
+             control = ctrl, eval.loglik = FALSE)
 summary(fit5)
 
 # ---- Your turn: add odegree(0:1) the same way -------------------------------
@@ -35,7 +40,7 @@ summary(fit5)
 # TODO: net_warm2 <- simulate(fit5, nsim = 1)
 # TODO: fit6 <- ergm(update(f_mix, net_warm2 ~ . + odegree(0:1)),
 #                    target.stats = c(ts_mix, odeg_target(0:1)),
-#                    eval.loglik = FALSE)
+#                    control = ctrl, eval.loglik = FALSE)
 # summary(fit6)
 
 # --- Try at home -------------------------------------------------------------
